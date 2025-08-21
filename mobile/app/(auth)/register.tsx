c2c5f87a746125af98fdceab523af9b34348696d
Ravindu Deshan Perera <deshr.perera03@gmail.com>
2025-08-22 00:09:27 +0530

    Empty branch

--- mobile/app/(auth)/register.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function Register() {
-  return (
-    <View className="flex-1 items-center justify-center bg-gray-100">
-      <Text className="text-xl font-semibold">Register Screen</Text>
-    </View>
-  );
-}
