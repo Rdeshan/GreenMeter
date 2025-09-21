74dfeb2393890098793efd079de04ffa0868bf56
Ravindu Deshan Perera <deshr.perera03@gmail.com>
2025-09-21 12:10:33 +0530

    nirmal,chamika backend
    complete

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,9 @@ import cors from 'cors';
 import helmet from 'helmet';
 import authRoutes from './routes/auth.routes';
 import deviceRoutes from './routes/device_route';   
+import energyCostRoutes from './routes/energyCost.routes';
+import healthRoutes from './routes/energyCost.routes';
+
 
 const app = express();
 
@@ -14,4 +17,8 @@ app.use(express.json());
 // Routes
 app.use('/auth', authRoutes);
 app.use('/api', deviceRoutes);
+app.use('/api/health', healthRoutes);
+app.use('/api/costs', energyCostRoutes);
+
+
 export default app;
